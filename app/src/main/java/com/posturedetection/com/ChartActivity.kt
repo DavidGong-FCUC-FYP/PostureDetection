51210c3bd2c9255dd5f66b2bf2bfad25de4641cc
David Gong <davidgonghongshen@gmail.com>
2023-07-23 21:00:39 +0800

    1

---
+++ app/src/main/java/com/posturedetection/com/ChartActivity.kt
@@ -0,0 +1,11 @@
+package com.posturedetection.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chart)
+    }
+}
