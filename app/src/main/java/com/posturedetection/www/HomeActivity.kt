5cac78ebccb3e81f7e376b7e36f2fcf572e9eefe
David Gong <davidgonghongshen@gmail.com>
2023-07-27 22:33:57 +0800

    have not finish bottom Navigation View

--- app/src/main/java/com/posturedetection/www/HomeActivity.kt
+++ app/src/main/java/com/posturedetection/www/HomeActivity.kt
@@ -6,6 +6,13 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
@@ -15,6 +22,29 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment_activity)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_detection,
+                R.id.navigation_statistics,
+                R.id.navigation_profile
+
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+        navView.getOrCreateBadge(R.id.navigation_detection)?.number = 2
+
+
+
+
+
+
+
+
 
 
 
@@ -33,4 +63,7 @@ class HomeActivity : AppCompatActivity() {
 //            startActivity(Intent(this , MainActivity::class.java))
 //        }
     }
+
+
+
 }
